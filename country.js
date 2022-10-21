c100195c0b1a7c682ef4a50a16ed9b60494b301a
Troy <wilson.troy.anthony@gmail.com>
2022-10-20 21:47:23 -0400

    Add project files.

---
+++ country.js
@@ -0,0 +1,37 @@
+class Country {
+    constructor(name, lang, greeting, colors) {
+        this.name = name;
+        this.lang = lang;
+        this.greeting = greeting;
+        this.colors = colors;
+    }
+}
+let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"]);
+let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
+let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
+
+
+function SwitchCountry() {
+    let input = document.getElementById("CountryList").value;
+    let country;
+
+    if (input === "USA") {
+        country = usa;   
+    }
+    else if (input === "Mexico") {
+        country = mexico;
+    }
+    else if (input === "Algeria") {
+        country = algeria;  
+    }  
+    DisplayColors(country);
+}
+
+function DisplayColors(country) {
+    document.getElementById("CountryName").innerHTML = country.name;
+    document.getElementById("OfficialLanguage").innerHTML = country.lang;
+    document.getElementById("HelloWorld").innerHTML = country.greeting;
+    document.getElementById("Color1").style.backgroundColor = country.colors[0];
+    document.getElementById("Color2").style.backgroundColor = country.colors[1];
+    document.getElementById("Color3").style.backgroundColor = country.colors[2];
+}
